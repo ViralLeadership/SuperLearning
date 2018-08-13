9d575ef644f7d09d54dacbce3a3e879786de4180
ViralLeadership <Dr_Jenkins@Outlook.com>
2018-08-13 17:54:55 -0400

    added a test commit

--- the_vim_way/playroom.py
+++ the_vim_way/playroom.py
@@ -1,10 +1,5 @@
-#import the numpy math package provided with Anaconda Python
-import numpy as np
-
-# the entire numpy package can be accessed through np
-print(np.sqrt(2))
-print(np.cos(0.707))
-print(np.tan(0.235))
-print()
-n = 2000
-print(2**(n-1))
+my_pets = ['raider','mustard','rainbow', 7]
+tabs = len(my_pets)
+for petnames in my_pets:
+    for tab in tabs:
+        print(petnames)
