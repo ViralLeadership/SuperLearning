d0351c4ad054564e485114f87ba7cbedfee1bb96
Jenkins <JenkiJ@CobbCounty.org>
2018-09-07 20:21:42 -0400

    first ITP add

---
+++ the_vim_way/func.py
@@ -0,0 +1,7 @@
+def iseven(n):
+    if n%2 == 0:
+        print("The number " + str(n) + " is even!")
+    else:
+        print("The number " + str(n) + " is odd.")
+
+iseven(14)
--- the_vim_way/tsla.py
+++
@@ -1,4 +0,0 @@
-import pandas_datareader as pdr
-tsla = pdr.DataReader("tsla", data_source = "google",
-        start = "2017-1-1")
-tsla.head(8)
